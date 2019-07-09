3df93d989ef99fa2cfa97e72aea78ff12444984d
JesusLinares01 <albertolinares001@gmail.com>
2019-07-09 09:51:22 -0600

    the get method was added and search

--- AppForms.py
+++ AppForms.py
@@ -14,7 +14,20 @@ db = SQLAlchemy(app)
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	username = db.Column(db.String(50), unique = True, nullable = False)
-	password = db.Column(db.String(50), nullable = False)		
+	password = db.Column(db.String(50), nullable = False)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/search")
+def search():
+	nickname = request.args.get("nickname")
+	user = Users.query.filter_by(username = nickname).first()
+
+	if user:
+		return "The user found succesfully is: " + user.username 
+	return "the user doesn't exist" 
 
 @app.route("/signup", methods = ["GET", "POST"])
 def signup():	
@@ -27,6 +40,18 @@ def signup():
 		return "You've registred succesfully"
 
 	return render_template("signup.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():	
+	if request.method == "POST":
+		user = Users.query.filter_by(username = request.form["username"]).first()
+
+		if user and check_password_hash(user.password, request.form["password"]):
+			return "You're logeed in"
+
+		return "Your data is invalid, check and try again"
+
+	return render_template("login.html")
 
 if __name__ == "__main__":
 	db.create_all()
